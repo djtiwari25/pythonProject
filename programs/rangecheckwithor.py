72938fbde1ca71c35e47635cd81e92e580f4fcfb
Dhananjay Tiwari <dhananjay.dataengineer@hotmail.com>
2024-08-22 00:12:40 +0530

    developed code for assignment 1

---
+++ programs/rangecheckwithor.py
@@ -0,0 +1,7 @@
+num = int(input("Enter a number:" ))
+
+if(num < -10 or num > 10):
+    print(num, "is either greater than 10 or less -10")
+
+else:
+    print(num, " is either less than 10 or greater than -10")
